701f4be1b0911c4fdc7daa6c3f65ead7465b30ac
luizs <luiz.souza.709@ufrn.edu.br>
2023-03-23 00:55:39 -0300

    criação dos models, forms e routes

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app.forms import LoginForm
+from flask import render_template, redirect, flash
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
+    
